74bff320c4b40f000329ca6a4d64964a4063f74c
amy <ctrlalternativedelete@gmail.com>
2022-10-11 17:59:04 +1100

    init

---
+++ tailwind.config.cjs
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const plugin = require('tailwindcss/plugin');
+
+module.exports = {
+    content: ["./src/**/*.{js,ts,jsx,tsx}"],
+    darkMode:"class",
+    theme: {
+        extend: {
+            colors: {
+                primary: "#e84db1",
+                secondary: "#714fac",
+                error: "#ff3863",
+                success: "#3dfb84"
+            }
+        },
+    },
+    plugins: [
+        plugin(({addVariant}) => {
+            addVariant('scroll0', ['html:not([scroll]) &', 'html[scroll="0"] &']);
+        })
+    ],
+};
