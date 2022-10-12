f1144ca6b38a5db9ace6f660e28e44352b1dde11
amy <ctrlalternativedelete@gmail.com>
2022-10-12 15:00:05 +1100

    added radio

---
+++ src/pages/radio.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from 'next';
+
+const Radio: NextPage = () => {
+    return (
+        <div className="pt-20 sm:px-36 p-8">
+            <iframe className="card p-0" src="https://open.spotify.com/embed/artist/15HdoPMP89EsIfIvN1coko?utm_source=generator&theme=0" width="100%" height="380" frameBorder="0" allowFullScreen allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
+        </div>
+    )
+}
+
+export default Radio;
