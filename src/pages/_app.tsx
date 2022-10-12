dc08dbfdc26753847dccb542402a23d4fd417ec1
amy <ctrlalternativedelete@gmail.com>
2022-10-12 14:50:44 +1100

    added scroll handler

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,12 +3,14 @@ import type { AppType } from "next/dist/shared/lib/utils";
 import Header from "components/Header";
 import { ThemeProvider } from "next-themes";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import ScrollHandler from "components/ScrollHandler";
 
 const queryClient = new QueryClient();
 
 const MyApp: AppType = ({ Component, pageProps }) => {
     return (
         <QueryClientProvider client={queryClient}>
+            <ScrollHandler />
             <ThemeProvider attribute="class">
                 <Header />
                 <Component {...pageProps} />
