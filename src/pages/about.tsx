9bda7323b94af89726f7c091340ca0b0f7b4edba
amy <ctrlalternativedelete@gmail.com>
2022-10-12 14:29:05 +1100

    Added loading

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -10,15 +10,18 @@ const About: NextPage = () => {
         const data = await fetch(`https://raw.githubusercontent.com/${process.env.GH_USERNAME}/${process.env.GH_USERNAME}/master/README.md`);
         return data.text();
     })
+    console.log()
 
     return (
         <div className="mt-20 m-8 md:px-32">
             <div className="card prose sm:p-12 dark:prose-invert prose-img:inline prose-img:m-0 max-w-full">
                 {
-                    mkdwn.data && (
+                    mkdwn.data ? (
                         <ReactMarkdown rehypePlugins={[rehypeRaw]}>
                             {mkdwn.data}
                         </ReactMarkdown>
+                    ) : (
+                        <>Loading...</>
                     )
                 }
             </div>
