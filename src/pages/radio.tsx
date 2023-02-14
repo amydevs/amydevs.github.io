f45954b3530b56e4af8bb73cdcb32cebec905899
Amy! <50583248+amydevs@users.noreply.github.com>
2023-02-14 19:24:11 +1100

    Update radio.tsx

--- src/pages/radio.tsx
+++ src/pages/radio.tsx
@@ -7,11 +7,11 @@ const Radio: NextPage = () => {
 
     return (
         <div className="pt-20 auto-limit-w">
-            <div className='card hover p-0 overflow-hidden relative bg-primary'>
+            <div className='card hover p-0 overflow-hidden relative'>
                 {
                     !loaded && <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'><Spinner className='fill-primary' /></div>
                 }
-                <iframe className={`${!loaded && 'invisible'}`} onLoad={() => {setLoaded(true)}} src="https://open.spotify.com/embed/artist/15HdoPMP89EsIfIvN1coko?utm_source=generator&theme=0" width="100%" height="380" frameBorder="0" allowFullScreen allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
+                <iframe className={`${!loaded && 'invisible'} bg-primary`} onLoad={() => {setLoaded(true)}} src="https://open.spotify.com/embed/artist/15HdoPMP89EsIfIvN1coko?utm_source=generator&theme=0" width="100%" height="380" frameBorder="0" allowFullScreen allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
             </div>
         </div>
     )
