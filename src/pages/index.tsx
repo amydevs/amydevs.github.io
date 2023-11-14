e534e963e6309cfdf0373aa009ac03010c0be6cc
Amy Y <ctrlalternativedelete@gmail.com>
2023-11-15 02:07:42 +1100

    fix: image now no longer has spinner

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,25 +4,14 @@ import Image from "next/image";
 import SocialLinks from 'cfg/social'
 import { ArrowDownIcon } from "@heroicons/react/24/solid";
 import Link from "next/link";
-import Spinner from "components/Spinner";
-import { useEffect, useState } from "react";
 
 const Home: NextPage = () => {
-    const [pfpLoaded, setPfpLoaded] = useState(true);
-    useEffect(() => {
-        setPfpLoaded(false); 
-    }, []);
 
     return (
         <div className="min-h-screen flex flex-col px-8 pb-8 lg:px-32 space-y-6">
             <div className="flex-1 flex flex-col justify-around items-center text-center gap-2 pt-16 sm:min-h-fit min-h-screen">
                 <div className="relative grid overflow-hidden rounded-full shadow hover:shadow-lg transition-all ease-in-out">
-                    <div 
-                        className="absolute h-full w-full flex items-center justify-center"
-                    >   
-                        <Spinner className={`fill-primary ${pfpLoaded && "hidden"}`} />
-                    </div>
-                    <Image className={`${!pfpLoaded && "opacity-0"} transition-all`} onLoad={() => setPfpLoaded(true)} width="460" height="460" alt="PFP" src={`https://github.com/${process.env.GH_USERNAME}.png`} />
+                    <Image width="460" height="460" alt="PFP" src={`https://github.com/${process.env.GH_USERNAME}.png`} />
                 </div>
                 <div>
                     <span className="text-3xl">Hi, I&apos;m <span className="text-primary">Amy</span>.</span> <br />
