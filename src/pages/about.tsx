f087b46408e21af36951fa482de0d17a656a7e1b
Amy Y <ctrlalternativedelete@gmail.com>
2023-11-15 01:52:22 +1100

    markdown is now gotten via static props

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,31 +1,26 @@
-import { NextPage } from 'next'
+import { GetStaticProps, InferGetStaticPropsType } from 'next'
 
 import ReactMarkdown from 'react-markdown'
 import rehypeRaw from 'rehype-raw'
 
-import { useQuery } from '@tanstack/react-query';
+export const getStaticProps = (async () => {
+    const res = await fetch(`https://raw.githubusercontent.com/${process.env.GH_USERNAME}/${process.env.GH_USERNAME}/master/README.md`)
+    const markdown = await res.text();
+    return { props: { markdown } }
+}) as GetStaticProps<{
+    markdown: string
+}>;
 
-const About: NextPage = () => {
-    const mkdwn = useQuery(["about"], async () => {
-        const data = await fetch(`https://raw.githubusercontent.com/${process.env.GH_USERNAME}/${process.env.GH_USERNAME}/master/README.md`);
-        return data.text();
-    })
-    console.log()
-
+const About = ({
+    markdown,
+  }: InferGetStaticPropsType<typeof getStaticProps>) => {
     return (
         <div className="mt-20 mb-4 auto-limit-w">
             <div className="card prose sm:p-12 dark:prose-invert prose-img:inline prose-img:m-0 max-w-full">
-                {
-                    mkdwn.data ? (
-                        <ReactMarkdown rehypePlugins={[rehypeRaw]}>
-                            {mkdwn.data}
-                        </ReactMarkdown>
-                    ) : (
-                        <>Loading...</>
-                    )
-                }
+                <ReactMarkdown rehypePlugins={[rehypeRaw]}>
+                    {markdown}
+                </ReactMarkdown>
             </div>
-            
         </div>
     )
 }
