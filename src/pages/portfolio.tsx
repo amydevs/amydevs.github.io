07e3c1f48186b30883f7b6bd04f51642ca1beeab
Amy Y <ctrlalternativedelete@gmail.com>
2024-03-06 12:40:33 +1100

    fix: scrolling

--- src/pages/portfolio.tsx
+++ src/pages/portfolio.tsx
@@ -6,6 +6,7 @@ import projectCards from "cfg/projectCards";
 import bentoCards from "cfg/bentoCards";
 
 import BentoCard from "components/BentoCard";
+import { ArrowLeftCircleIcon } from "@heroicons/react/24/solid";
 import { useEffect, useState, useRef } from "react";
 import { cn } from "utils/cn";
 
@@ -27,9 +28,14 @@ const Portfolio: NextPage = () => {
     
     return(
         <div className="space-y-3">
-            <div ref={bentoScrollWrapperRef} className={cn(bentoScroll != null && "focus-within:overflow-x-hidden", "overflow-x-auto h-screen sm:h-fit pt-16 px-0 flex sm:block sm:auto-limit-w")}>
-                <div className="min-w-[768px] sm:min-w-min flex-1 relative w-full aspect-video sm:rounded-lg overflow-hidden">
-                    { bentoCards.map(({...props}, i) => <BentoCard scrollY={bentoScroll} key={i} {...props} />) }
+            <div className="pt-16 px-0 sm:auto-limit-w">
+                <div className="relative overflow-hidden">
+                    <div ref={bentoScrollWrapperRef} className={cn(bentoScroll != null && "focus-within:overflow-x-hidden", "overflow-x-auto h-[calc(100vh-4rem)] sm:h-fit flex sm:block peer")}>
+                        <div className="min-w-[768px] sm:min-w-min flex-1 relative w-full aspect-video sm:rounded-lg overflow-hidden">
+                            { bentoCards.map(({...props}, i) => <BentoCard scrollY={bentoScroll} key={i} {...props} />) }
+                        </div>
+                    </div>
+                    <ArrowLeftCircleIcon className="absolute z-10 right-8 -top-12 peer-focus-within:top-8 text-white h-12 w-12 rounded-full bg-slate-950/50 cursor-pointer transition-all" />
                 </div>
             </div>
             <div className="pb-2 auto-limit-w space-y-3">
