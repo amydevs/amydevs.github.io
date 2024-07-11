cbe5860dc04d289999d28c1ebdd963f66da0487a
Amy Yan <ayanamydev@gmail.com>
2024-07-11 23:10:38 +1000

    fix: radio bg

--- src/pages/radio.tsx
+++ src/pages/radio.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import { LoaderCircle } from 'lucide-react';
 import { useState } from 'react';
-import { Card } from '~/components/ui/card';
 import { cn } from '~/lib/utils';
 
 const Radio = () => {
@@ -10,12 +9,12 @@ const Radio = () => {
 
   return (
     <div className='flex py-3 auto-limit-w min-h-[calc(100vh-5rem)]'>
-      <Card className="flex flex-1 overflow-hidden">
+      <div className="flex flex-1 overflow-hidden">
         {
           !loaded && <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'><LoaderCircle className='text-primary animate-spin' /></div>
         }
         <iframe ref={ref} className={cn("flex-1", !loaded && "invisible")} onLoad={() => {setLoaded(true)}} src="https://open.spotify.com/embed/artist/15HdoPMP89EsIfIvN1coko?utm_source=generator&theme=0" width="100%" frameBorder="0" allowFullScreen allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
-      </Card>
+      </div>
     </div>
         
   )
