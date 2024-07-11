c88b1eccd3727ecdab5d7bb3ff0e0e836822ff3e
Amy Yan <ayanamydev@gmail.com>
2024-07-11 21:03:57 +1000

    fix: html decoding on about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -4,30 +4,33 @@ import { serialize } from 'next-mdx-remote/serialize'
 import { MDXRemote,  } from 'next-mdx-remote'
 import { env } from "~/env";
 import rehypeRaw from 'rehype-raw';
+import * as he from 'he';
 
 const getStaticProps: GetStaticProps<{
   mdxSource: MDXRemoteSerializeResult;
 }> = async () => {
   const repo = `${env.NEXT_PUBLIC_GH_USER}/${env.NEXT_PUBLIC_GH_USER}`
   const res = await fetch(`https://raw.githubusercontent.com/${repo}/master/README.md`)
-  const source = await res.text();
+  let source = await res.text();
 
-  const imageUrls = [];
-  for (const match of source.matchAll(/<img.*?src=['"](.*?)['"]/gi)) {
-    if (match[1] != null) {
-      imageUrls.push(match[1].trim());
+  for (const match of source.matchAll(/(?:<img.*?src=['"](.+?)['"])|(?:\[.*?\]\((.+?)\))/gi)) {
+    let unescapedUrl = match.at(1) ?? match.at(2);
+    if (unescapedUrl == null) {
+      continue;
     }
+    unescapedUrl = unescapedUrl.trim();
+    source = source.replaceAll(unescapedUrl, he.decode(unescapedUrl));
   }
 
   const profileRes = await fetch(`https://github.com/${repo}`);
   const profile = await profileRes.text();
-  for (const match of profile.matchAll(/src="(.*?)" data-canonical-src="(.*?)"/g)) {
+  for (const match of profile.matchAll(/src="(.*?)".*?data-canonical-src="(.*?)"/g)) {
     if (match[1] == null || match[2] == null) {
       continue;
     }
-    const camoUrl = decodeURI(match[1].trim());
-    const realUrl = decodeURI(match[2].trim());
-    source.replace(realUrl, camoUrl);
+    const camoUrl = he.decode(match[1].trim());
+    const realUrl = he.decode(match[2].trim());
+    source = source.replaceAll(realUrl, camoUrl);
   }
 
   const mdxSource = await serialize(source, { mdxOptions: { format: "md", rehypePlugins: [rehypeRaw] } })
