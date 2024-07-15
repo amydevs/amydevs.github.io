c43bebe88d261334eec8e84f80d6c8d50711a19a
Amy Y <ctrlalternativedelete@gmail.com>
2024-07-16 00:08:46 +1000

    feat: added frontmatter

--- src/schemas.ts
+++ src/schemas.ts
@@ -3,9 +3,10 @@ import * as z from 'zod';
 const postFrontmatter = z.object({
   title: z.string(),
   topic: z.string().optional(),
-  created: z.string().datetime(),
-  updated: z.string().datetime().optional(),
+  date: z.string().datetime(),
+  lastModified: z.string().datetime().optional(),
   description: z.string(),
+  draft: z.boolean().optional(),
 });
 
 export {
