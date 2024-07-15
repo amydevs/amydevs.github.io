e76778ba99ed9946379018385c8af348497e4a89
Amy Yan <ayanamydev@gmail.com>
2024-07-15 17:10:48 +1000

    fix: footer text-md is not real

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -12,7 +12,7 @@ const Footer = React.forwardRef<
     <div ref={ref} className={cn('flex flex-col items-center justify-between gap-4 py-10 md:h-24 md:flex-row md:py-0', className)} {...props}>
       <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
         <Image src="/favicon-32x32.png" width={32} height={32} alt="logo" />
-        <p className="text-center text-md leading-loose md:text-left">
+        <p className="text-center leading-loose md:text-left">
           Built by{" "}
           <Link className="underline text-primary" href={`https://github.com/${env.NEXT_PUBLIC_GH_USER}`}>
             { env.NEXT_PUBLIC_GH_USER }
