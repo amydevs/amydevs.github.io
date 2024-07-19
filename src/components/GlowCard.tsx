91abfe126fe1a43d600ebe6924797fab509b0ea2
Amy Yan <ayanamydev@gmail.com>
2024-07-19 13:30:39 +1000

    fix: prettier classnames

--- src/components/GlowCard.tsx
+++ src/components/GlowCard.tsx
@@ -18,7 +18,13 @@ const GlowCard = React.forwardRef<
     <Card
       ref={cardRef}
       className={cn(
-        `relative z-10 overflow-hidden transition-all after:absolute after:left-[var(--x)] after:top-[var(--y)] after:-z-10 after:h-[100rem] after:w-[100rem] after:-translate-x-1/2 after:-translate-y-1/2 after:bg-[radial-gradient(hsl(var(--primary)/10%),#3984ff00_70%)] after:transition-[height,width] after:duration-700 after:ease-out after:content-[''] dark:after:bg-[radial-gradient(hsl(var(--primary)/30%),#3984ff00_70%)]`,
+        `relative z-10 overflow-hidden transition-all after:absolute
+        after:left-[var(--x)] after:top-[var(--y)] after:-z-10 after:h-[100rem]
+        after:w-[100rem] after:-translate-x-1/2 after:-translate-y-1/2
+        after:bg-[radial-gradient(hsl(var(--primary)/10%),#3984ff00_70%)]
+        after:transition-[height,width] after:duration-700 after:ease-out
+        after:content-['']
+        dark:after:bg-[radial-gradient(hsl(var(--primary)/30%),#3984ff00_70%)]`,
         x < 0 && y < 0 && "after:h-0 after:w-0",
         className,
       )}
