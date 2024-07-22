538a98fa913e9dde5347d774f1d47be6fb6c36fb
Amy Yan <ayanamydev@gmail.com>
2024-07-22 14:53:13 +1000

    fix: footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -12,7 +12,7 @@ const Footer = React.forwardRef<
     <div
       ref={ref}
       className={cn(
-        `flex flex-col items-center justify-between gap-4 py-10 md:h-24·md:flex-row
+        `flex flex-col items-center justify-between gap-4 py-10 md:h-24 md:flex-row
         md:py-0`,
         className,
       )}
